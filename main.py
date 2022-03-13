d3d9287ce0c37d12a36f3a48466bc74fa60f15f0
Titouan-dev <84125457+Titouan-dev@users.noreply.github.com>
2022-03-13 14:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,3 @@ model = lp.Detectron2LayoutModel('lp://PrimaLayout/mask_rcnn_R_50_FPN_3x/config'
 # image = Image.open("path/to/image")
 print(model)
 layout = model.detect(image)
-lp.draw_box(image, layout, box_width=3)
