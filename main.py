f3e3e8c1ad7909128c0fb4d026ab1ff438d785e1
Titouan-dev <84125457+Titouan-dev@users.noreply.github.com>
2022-03-13 14:15:41 +0100

    1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import layoutparser as lp
 import pdf2image
 from PIL import Image
-image = pdf2image.convert_from_path("CM2 CM3 VRAI_compressed.pdf")
+image = Image.open("test.jpg")
 model = lp.Detectron2LayoutModel('lp://PrimaLayout/mask_rcnn_R_50_FPN_3x/config')
 # image = Image.open("path/to/image")
 print(model)
-layout = model.detect(image[0])
+layout = model.detect(image)
 print(layout)
